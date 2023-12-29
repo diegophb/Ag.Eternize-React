9eb5bbda32e4e55919c0056c339ad8bdb9d7ed2a
diegophb <prdiegopi@gmail.com>
2023-12-29 19:16:52 -0300

    Pg.Home

--- eternize/components/WelcomeScreen.js
+++ eternize/components/WelcomeScreen.js
@@ -1,26 +1,31 @@
+// WelcomeScreen.js
 import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import style from "../styles/WelcomeScreen.module.css";
-import { Container, Row, Col, Card } from "react-bootstrap";
 
 const placesData = [
   {
     name: "Maldivas",
     image: "/img/01.jpg",
+    description: "Explore as praias exóticas e as águas cristalinas das Maldivas.",
   },
   {
     name: "Curitiba",
     image: "/img/nacional/curitiba.jpg",
+    description: "Descubra a beleza e a cultura de Curitiba, uma cidade encantadora.",
   },
   {
     name: "Emirates",
     image: "/img/03.jpg",
+    description: "Viva a experiência luxuosa e vibrante dos Emirados Árabes Unidos.",
   },
   {
     name: "J.Pessoa",
     image: "/img/nacional/joao.jpg",
+    description: "Relaxe nas praias paradisíacas de João Pessoa, na Paraíba.",
   },
 ];
 
@@ -32,7 +37,9 @@ const WelcomeScreen = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 5000,
+    fade: true,
+    cssEase: "linear",
   };
 
   return (
@@ -41,11 +48,14 @@ const WelcomeScreen = () => {
         <h1 className={style.title}>Bem-vindo à Nossa Agência de Viagens</h1>
       </Container>
       <Container className={style.sliderContainer}>
-        <Slider {...sliderSettings}>
+        <Slider {...sliderSettings} className={style.slider}>
           {placesData.map((place, index) => (
             <div key={index} className={style.slideItem}>
               <img src={place.image} alt={place.name} className={style.slideImage} />
-              <p className={style.placeName}>{place.name}</p>
+              <div className={style.slideContent}>
+                <h3 className={style.placeName}>{place.name}</h3>
+                <p className={style.placeDescription}>{place.description}</p>
+              </div>
             </div>
           ))}
         </Slider>
@@ -54,10 +64,14 @@ const WelcomeScreen = () => {
         <Row>
           {placesData.map((place, index) => (
             <Col key={index} md={3} sm={6} xs={12} className={style.cardItem}>
-              <Card>
+              <Card className={style.card}>
                 <Card.Img variant="top" src={place.image} alt={place.name} className={style.cardImage} />
                 <Card.Body>
                   <Card.Title className={style.placeName}>{place.name}</Card.Title>
+                  <Card.Text className={style.placeDescription}>{place.description}</Card.Text>
+                  <Button variant="primary" className={style.learnMoreButton}>
+                    Saiba Mais
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
